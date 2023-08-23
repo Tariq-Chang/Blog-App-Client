9e3f7036d76c143786942613844745398f5930d8
Tariq Chang <tariqhussainchang363@gmail.com>
2023-08-23 11:16:20 +0500

    basic frontend setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img src="/media/tariq/D/Blogs-App/server/src/uploads/2023-08-19T14:24:11.748Zexpress.png" alt="Express" />
+    </>
+  )
+}
+
+export default App
