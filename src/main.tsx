f45db6113c4c42531c22ff81a78623d763e6c29d
Tariq Chang <tariqhussainchang363@gmail.com>
2023-08-23 15:30:52 +0500

    setup for react tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import App from './App.tsx'
 import './index.css'
 import { store } from './redux/store/store.ts'
 import { Provider } from 'react-redux'
-
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false}/>
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
 )
