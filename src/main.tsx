e05ebcc917a5f984a1e1523dd77d27319aaa39f7
Tariq Chang <tariqhussainchang363@gmail.com>
2023-09-18 22:00:53 +0500

    added redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { store } from './redux/store/store.ts'
-import { Provider } from 'react-redux'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/Router.tsx'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { persistor, store } from "./redux/store/store.ts";
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Router.tsx";
+import { PersistGate } from "redux-persist/integration/react";
+import { HashLoader } from "react-spinners";
 
 const queryClient = new QueryClient();
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <PersistGate loading={<HashLoader/>} persistor={persistor}>
       <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router}/>
+        <RouterProvider router={router} />
         <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
       </QueryClientProvider>
-    </Provider>
-)
+    </PersistGate>
+  </Provider>
+);
--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,10 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userSlice from "../features/userSlice";
 import blogSlice from "../features/blogSlice";
 
-export const store = configureStore({
-    reducer: {
-        user: userSlice,
-        blogs: blogSlice
-    }
-})
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userSlice,
+  blogs: blogSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({reducer: persistedReducer});
+export let persistor = persistStore(store);
+
