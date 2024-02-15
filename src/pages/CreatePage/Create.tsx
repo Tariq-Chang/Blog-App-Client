2d649bc761056c27b7227bc4732f9244291dff7e
Tariq Hussain Chang <tariqhussainchang363@gmail.com>
2024-02-15 11:03:09 +0500

    fixed textarea autosize and api thumbnail api implement

--- src/pages/CreatePage/Create.tsx
+++ src/pages/CreatePage/Create.tsx
@@ -1,17 +1,53 @@
 import { IoMdAddCircleOutline } from "react-icons/io";
 import { CiImageOn } from "react-icons/ci";
 import { CiVideoOn } from "react-icons/ci";
-import { useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
+import axios from "axios";
+import Cookies from 'js-cookie'
 
 export default function Create() {
     const [showAddMedia, setShowAddMedia] = useState(false);
+    const [thumbnailFile, setThumbnailFile] = useState();
+    const [content, setContent] = useState('');
+    const url = "http://localhost:5000/api/v1/blogs/addBlogThumbnail";
+    const textAreaRef = useRef();
+    const handleThumbnailChange = async (e: ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files;
+        const token = Cookies.get('jwtToken');
+        const formData = new FormData();
+        formData.append('thumbnail', files[0])
+        try {
+            const response = await axios.post(url, formData, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            console.log("Response: ", response);
+            setThumbnailFile(response?.data?.img_url);
+        } catch (error) {
+            console.log('ERROR: ', error)
+        }
+    }
+
+    const handleTextAreaChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+        setContent(event.target.value);
+        adjustTextareaHeight();
+      };
+    
+      const adjustTextareaHeight = () => {
+        if (textAreaRef.current) {
+          textAreaRef.current.style.height = 'auto';
+          textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+        }
+      };
+    
     return (
         <div className="w-[60%] mx-auto">
             {/* Thumbnail */}
-            <div >
-                <input type="file" name="" id="thumbnail" className="invisible w-full h-full" />
+            <div>
+                <input type="file" name="thumbnail" id="thumbnail" className="invisible w-full h-full" onChange={handleThumbnailChange} />
                 <label htmlFor="thumbnail">
-                    <div className="flex justify-center items-center h-56 border-2 bg-gray-200 border-gray-400 rounded-lg cursor-pointer hover:bg-gray-300">
+                    <div className={`flex justify-center items-center h-56 border-2 bg-gray-100 border-gray-400 rounded-lg cursor-pointer bg-no-repeat bg-contain bg-center hover:bg-gray-200`} style={{ backgroundImage: `url(${thumbnailFile})` }}>
                         <IoMdAddCircleOutline className="text-gray-500 w-16 h-16" />
                     </div>
                 </label>
@@ -36,10 +72,12 @@ export default function Create() {
                             <CiVideoOn className="text-2xl text-blue-600 cursor-pointer" />
                         </label>
                     </div>}
-                    <textarea name="content" id="content"
-                        rows="1"
+
+                    <textarea id="content"
+                        ref={textAreaRef}
+                        onChange={handleTextAreaChange}
                         placeholder="Share your thoughts"
-                        className="w-full text-xl text-gray-600 ml-2 border-none placeholder:text-gray-400 focus:ring-0"></textarea>
+                        className="w-full overflow-hidden resize-none text-xl text-gray-600 ml-2 border-none placeholder:text-gray-400 focus:ring-0"></textarea>
                 </div>
                 <button className="absolute right-0 mt-8 bg-gray-800 text-white px-4 py-2 rounded-lg drop-shadow-lg w-32 hover:bg-gray-900">Post</button>
             </div>
