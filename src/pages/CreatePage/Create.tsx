0d630924ee2fe5bb33bde55ca1c173f13d01c1ad
Tariq Hussain Chang <tariqhussainchang363@gmail.com>
2024-02-15 14:17:51 +0500

    implemented create blog api

--- src/pages/CreatePage/Create.tsx
+++ src/pages/CreatePage/Create.tsx
@@ -1,15 +1,25 @@
 import { IoMdAddCircleOutline } from "react-icons/io";
 import { CiImageOn } from "react-icons/ci";
 import { CiVideoOn } from "react-icons/ci";
-import { ChangeEvent, useRef, useState } from "react";
+import React, { ChangeEvent, useRef, useState } from "react";
 import axios from "axios";
 import Cookies from 'js-cookie'
+import { setMyBlogs } from "../../redux/features/blogSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function Create() {
+    const dispatch = useDispatch();
+    const myBlogs = useSelector((state: any) => state.blogs.myBlogs)
+    const navigate = useNavigate();
     const [showAddMedia, setShowAddMedia] = useState(false);
-    const [thumbnailFile, setThumbnailFile] = useState();
-    const [content, setContent] = useState('');
-    const url = "http://localhost:5000/api/v1/blogs/addBlogThumbnail";
+    const [post, setPost] = useState({
+        thumbnail: '',
+        title: '',
+        content: '',
+    })
+
+    const url = "http://localhost:5000/api/v1/blogs";
     const textAreaRef = useRef();
     const handleThumbnailChange = async (e: ChangeEvent<HTMLInputElement>) => {
         const files = e.target.files;
@@ -17,37 +27,52 @@ export default function Create() {
         const formData = new FormData();
         formData.append('thumbnail', files[0])
         try {
-            const response = await axios.post(url, formData, {
+            const response = await axios.post(url + '/addBlogThumbnail', formData, {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
             })
             console.log("Response: ", response);
-            setThumbnailFile(response?.data?.img_url);
+            setPost((prevPost) => {
+                return { ...prevPost, thumbnail: response?.data.img_url }
+            })
         } catch (error) {
             console.log('ERROR: ', error)
         }
     }
 
-    const handleTextAreaChange = (event:React.ChangeEvent<HTMLInputElement>) => {
-        setContent(event.target.value);
+    const handleTextAreaChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPost({ ...post, content: event.target.value })
         adjustTextareaHeight();
-      };
-    
-      const adjustTextareaHeight = () => {
+    };
+
+    const adjustTextareaHeight = () => {
         if (textAreaRef.current) {
-          textAreaRef.current.style.height = 'auto';
-          textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+            textAreaRef.current.style.height = 'auto';
+            textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
         }
-      };
-    
+    };
+
+    const handleCreatePost = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const token = Cookies.get('jwtToken');
+        const response = await axios.post(url + '/create', post, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        console.log("post", response.data);
+        dispatch(recentBlog(post))
+        dispatch(setMyBlogs({ ...myBlogs, post }))
+        navigate('/dashboard');
+    }
     return (
         <div className="w-[60%] mx-auto">
             {/* Thumbnail */}
             <div>
                 <input type="file" name="thumbnail" id="thumbnail" className="invisible w-full h-full" onChange={handleThumbnailChange} />
                 <label htmlFor="thumbnail">
-                    <div className={`flex justify-center items-center h-56 border-2 bg-gray-100 border-gray-400 rounded-lg cursor-pointer bg-no-repeat bg-contain bg-center hover:bg-gray-200`} style={{ backgroundImage: `url(${thumbnailFile})` }}>
+                    <div className={`flex justify-center items-center h-56 border-2 bg-gray-100 border-gray-400 rounded-lg cursor-pointer bg-no-repeat bg-contain bg-center hover:bg-gray-200`} style={{ backgroundImage: `url(${post.thumbnail})` }}>
                         <IoMdAddCircleOutline className="text-gray-500 w-16 h-16" />
                     </div>
                 </label>
@@ -55,7 +80,7 @@ export default function Create() {
 
             <div className="relative">
                 {/* Title */}
-                <input type="text" name="title" placeholder="Title" className="border-none text-4xl text-gray-600 m-2 focus:outline-none  focus:ring-0 placeholder:text-gray-400" />
+                <input type="text" onChange={(e) => setPost({ ...post, title: e.target.value })} value={post.title} name="title" placeholder="Title" className="border-none text-4xl text-gray-600 m-2 focus:outline-none  focus:ring-0 placeholder:text-gray-400" />
                 {/* Content and Actions */}
                 <div className="flex">
                     <button className="absolute -bottom-10 left-2 rounded-full p-2 text-gray-500 hover:text-gray-800">
@@ -74,12 +99,17 @@ export default function Create() {
                     </div>}
 
                     <textarea id="content"
+                        value={post.content}
                         ref={textAreaRef}
                         onChange={handleTextAreaChange}
                         placeholder="Share your thoughts"
                         className="w-full overflow-hidden resize-none text-xl text-gray-600 ml-2 border-none placeholder:text-gray-400 focus:ring-0"></textarea>
                 </div>
-                <button className="absolute right-0 mt-8 bg-gray-800 text-white px-4 py-2 rounded-lg drop-shadow-lg w-32 hover:bg-gray-900">Post</button>
+                <button
+                    type="submit"
+                    onClick={handleCreatePost}
+                    disabled={!post.thumbnail}
+                    className="absolute right-0 mt-8 bg-gray-800 text-white px-4 py-2 rounded-lg drop-shadow-lg w-32 hover:bg-gray-900 disabled:bg-gray-600">Post</button>
             </div>
         </div>
     )
