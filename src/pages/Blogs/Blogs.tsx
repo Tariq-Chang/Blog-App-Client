3c91cf549092ec31390b8a361c4732e9c0fca564
Tariq Hussain Chang <tariqhussainchang363@gmail.com>
2024-02-23 12:52:51 +0500

    added heading for blogs

--- src/pages/Blogs/Blogs.tsx
+++ src/pages/Blogs/Blogs.tsx
@@ -3,9 +3,9 @@ import Cards from '../../layout/Cards/Cards'
 
 function Blogs() {
   const blogs = useSelector((state: any) => state.blogs.blogs);
-    console.log("blogs", blogs);
   return (
     <div>
+      <h1 className="text-3xl mb-6 tracking-wide text-blue-600">All Blogs</h1>
         {blogs?.length > 0 ? <Cards blogs={blogs}/> : <h1 className="text-2xl">Blogs not found!</h1>}
     </div>
   )
--- src/pages/MyBlogs/MyBlogs.tsx
+++ src/pages/MyBlogs/MyBlogs.tsx
@@ -6,6 +6,7 @@ function MyBlogs() {
 
   return (
     <div>
+      <h1 className="text-3xl mb-6 tracking-wide text-blue-600">My Blogs</h1>
         {(myBlogs && myBlogs.length > 0) ? <Cards blogs={myBlogs}/> : <h1 className="text-2xl">User has not written any Blog</h1>}
     </div>
   )
