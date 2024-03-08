e2f475aa7eddad9b9604eaf13bedf9d4449ab4a9
Tariq Hussain Chang <tariqhussainchang363@gmail.com>
2024-03-08 14:56:57 +0500

    replaced savedBlogs from api call to redux state

--- src/pages/Bookmarks/Bookmarks.tsx
+++ src/pages/Bookmarks/Bookmarks.tsx
@@ -1,38 +1,18 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Cookies from 'js-cookie'
 import Cards from "../../layout/Cards/Cards";
-import { Blog } from "../../interfaces/Blog";
-import { useDispatch } from "react-redux";
-import { saveBlogs } from "../../redux/features/blogSlice";
+import { useSelector } from "react-redux";
 
 const Bookmarks = () => {
-    const dispatch = useDispatch();
-    const [savedBlogs, setSavedBlogs] = useState<Blog[]>()
-    const url = "http://localhost:5000/api/v1/blogs";
-    const fetchSavedBlogs = async() => {
-        const token = Cookies.get('jwtToken');
-        const response = await axios.get(url + '/savedBlogs', {
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
-        
-        setSavedBlogs(response.data[0].savedBlogs);
-        dispatch(saveBlogs(response.data[0].savedBlogs))
-
-    }
-    useEffect(() => {
-        fetchSavedBlogs();
-    }, [])
-    return (
-        <div className="p-8">
-            <h1 
-                className="text-3xl mb-6 tracking-wide text-blue-600">Bookmarks</h1>
-                {(savedBlogs && savedBlogs.length > 0) ? 
-                <Cards blogs={savedBlogs} /> : <h1 className="text-2xl">User has no Blog</h1>}
-        </div>
-    )
-}
+  const savedBlogs = useSelector((state: any) => state.blogs.savedBlogs);
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl mb-6 tracking-wide text-blue-600">Bookmarks</h1>
+      {savedBlogs && savedBlogs.length > 0 ? (
+        <Cards blogs={savedBlogs} />
+      ) : (
+        <h1 className="text-2xl">User has no Blog</h1>
+      )}
+    </div>
+  );
+};
 
 export default Bookmarks;
